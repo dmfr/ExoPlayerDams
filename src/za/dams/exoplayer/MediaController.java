f55689ddc8d2c7ffe6336742eb2596f61dd4289f
DM Laptop <dm@mirabel-sil.com>
2016-01-27 17:04:23 +0100

    MediaController: add dummy btn

--- src/za/dams/exoplayer/MediaController.java
+++ src/za/dams/exoplayer/MediaController.java
@@ -6,12 +6,36 @@ import com.google.android.exoplayer.util.PlayerControl;
 
 import android.content.Context;
 import android.os.Handler;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.ImageButton;
 
 public class MediaController extends android.widget.MediaController {
+	Context mContext ;
+	private ImageButton searchButton;
+	
 	public MediaController(Context context) {
 		super(context);
+		mContext = context.getApplicationContext() ;
 	}
+	
+	@Override 
+	public void setAnchorView(View view) {
+		super.setAnchorView(view);
 
+		searchButton = new ImageButton(mContext,null,android.R.style.MediaButton);
+		searchButton.setImageResource(android.R.drawable.ic_media_play);
+
+		FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+		params.gravity = Gravity.RIGHT;
+		params.rightMargin = 50 ;
+		params.topMargin = 25 ;
+		ViewGroup innerViewGroup = (ViewGroup)((ViewGroup)getChildAt(0)).getChildAt(0) ;
+		addView(searchButton, params);
+	}
+	
 	public void setMediaPlayer(MediaPlayerControl player) {
 		// TODO Auto-generated method stub
 		super.setMediaPlayer(player);
